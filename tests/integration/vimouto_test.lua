598d491084145c63916a80cfd307609d738247ec
jonesangga <jonesangga@proton.me>
2025-11-27 14:17:56 +0700

    test vimouto o, O, a, A bindings

--- tests/integration/vimouto_test.lua
+++ tests/integration/vimouto_test.lua
@@ -12,10 +12,7 @@ local fake = {
 }
 
 love.keyboard.isDown = function(k)
-    if fake[k] ~= nil then
-        return fake[k]
-    end
-    error("no key: " .. k)
+    return fake[k] ~= nil
 end
 
 love.graphics.setDefaultFilter("nearest", "nearest")  -- Nearest neighbor filtering for pixel art.
@@ -27,6 +24,7 @@ fsm.push(vimouto)
 
 describe("i binding", function()
     before(function()
+        fake = {}
         vimouto:reset()
     end)
 
@@ -41,6 +39,7 @@ end)
 
 describe("I binding", function()
     before(function()
+        fake = {}
         vimouto:reset()
     end)
 
@@ -63,6 +62,112 @@ describe("I binding", function()
         expect(vimouto.mode).to.equal("INSERT")
         expect(vimouto.active.blocked_chars["I"]).to.equal(true)
         expect(vimouto.active.cx).to.equal(1)
+    end)
+end)
+
+describe("o binding", function()
+    before(function()
+        fake = {}
+        vimouto:reset()
+    end)
+
+    it("normal become insert mode", function()
+        expect(vimouto.mode).to.equal("NORMAL")
+        fsm.keypressed("o")
+
+        expect(vimouto.mode).to.equal("INSERT")
+        expect(vimouto.active.blocked_chars["o"]).to.equal(true)
+        expect(vimouto.active.cy).to.equal(2)
+    end)
+
+    it("start at beginning", function()
+        vimouto.active.lines = {"a test line"}
+        vimouto.active.cx = 4
+        fsm.keypressed("o")
+
+        expect(vimouto.mode).to.equal("INSERT")
+        expect(vimouto.active.cx).to.equal(1)
+        expect(vimouto.active.cy).to.equal(2)
+    end)
+end)
+
+describe("O binding", function()
+    before(function()
+        fake = {}
+        vimouto:reset()
+    end)
+
+    it("normal become insert mode", function()
+        expect(vimouto.mode).to.equal("NORMAL")
+        fake.lshift = true
+        fsm.keypressed("o")
+
+        expect(vimouto.mode).to.equal("INSERT")
+        expect(vimouto.active.blocked_chars["O"]).to.equal(true)
+        expect(#vimouto.active.lines).to.equal(2)
+    end)
+
+    it("start at beginning", function()
+        vimouto.active.lines = {"a test line"}
+        vimouto.active.cx = 4
+        fake.lshift = true
+        fsm.keypressed("o")
+
+        expect(vimouto.mode).to.equal("INSERT")
+        expect(vimouto.active.blocked_chars["O"]).to.equal(true)
+        expect(vimouto.active.cx).to.equal(1)
+    end)
+end)
+
+describe("a binding", function()
+    before(function()
+        fake = {}
+        vimouto:reset()
+    end)
+
+    it("empty line", function()
+        expect(vimouto.mode).to.equal("NORMAL")
+        fsm.keypressed("a")
+
+        expect(vimouto.mode).to.equal("INSERT")
+        expect(vimouto.active.blocked_chars["a"]).to.equal(true)
+    end)
+
+    it("non empty line", function()
+        vimouto.active.lines = {"a test line"}
+        vimouto.active.cx = 4
+        fsm.keypressed("a")
+
+        expect(vimouto.mode).to.equal("INSERT")
+        expect(vimouto.active.cx).to.equal(5)
+    end)
+end)
+
+describe("A binding", function()
+    before(function()
+        fake = {}
+        vimouto:reset()
+    end)
+
+    it("empty line", function()
+        expect(vimouto.mode).to.equal("NORMAL")
+        fake.lshift = true
+        fsm.keypressed("a")
+
+        expect(vimouto.mode).to.equal("INSERT")
+        expect(vimouto.active.blocked_chars["A"]).to.equal(true)
+        expect(vimouto.active.cx).to.equal(1)
+    end)
+
+    it("start at beginning", function()
+        vimouto.active.lines = {"a test line"}
+        vimouto.active.cx = 4
+        fake.lshift = true
+        fsm.keypressed("a")
+
+        expect(vimouto.mode).to.equal("INSERT")
+        expect(vimouto.active.blocked_chars["A"]).to.equal(true)
+        expect(vimouto.active.cx).to.equal(12)
     end)
 end)
 
